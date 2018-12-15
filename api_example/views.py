6ee5f32e62cc97702fd776677968c14cc57d8b93
Paul Dexter-Sobkowiak <536437+krubokrubo@users.noreply.github.com>
2018-12-15 21:03:05 +0000

    Completed step 4 so that /api_example/ returns a basic JSON object

--- api_example/views.py
+++ api_example/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def names(request):
+    return JsonResponse({'names': ['William', 'Rod', 'Grant']})
