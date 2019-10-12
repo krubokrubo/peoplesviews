7471f7af9cf0c8b5e2e44bf614361712e268ed0e
Paul <you@example.com>
2019-10-12 17:58:23 +0200

    Change to use postgres on Heroku

--- config/settings.py
+++ config/settings.py
@@ -74,12 +74,18 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# Sqlite3 for local development
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# Postgresql for Heroku
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
